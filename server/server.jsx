e42d8ca2a4365d9e5856ca1f843e314d9244ed6c
Nicola Zilio <nicolazilio@gmail.com>
2023-11-06 15:30:18 +0100

    Clean up server code

--- server/server.jsx
+++ server/server.jsx
@@ -11,38 +11,34 @@ const app = express();
 
 app.get("/", (req, res) => {
 
-  fs.readFile(path.resolve("./public/plasmidMap.svg"), "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
+  fs.readFile(path.resolve("./public/plasmidMap.svg"), "utf8", (error, data) => {
+    
+    if (error) {
       return res.status(500).send("An error occurred");
     }
 
-    // Extract the SVG element from the rendered circular view
-    circularViewHtml = ReactDOMServer.renderToString(<App params={req.query} />);
-    const svgStartPos = circularViewHtml.indexOf('<svg style="overflow:visible;display:block" ') + '<svg style="overflow:visible;display:block" '.length;
-    const svgEndPos = circularViewHtml.indexOf("</svg>") + "<svg>".length + 1;
-    const style = 'style="overflow:visible;display:block;text-align:center;' +
-                  'font-family:-apple-system,BlinkMacSystemFont,Segoe UI,' +
-                  'Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,' + 
-                  'Helvetica Neue,sans-serif" ';
-    let svgElement = '<svg id="plasmidMap" ' + style + circularViewHtml.substring(svgStartPos, svgEndPos).trim();
+    try {
 
-    // Get rid of hand pointers when hovering over a feature
-    svgElement = svgElement.replaceAll('style="cursor:pointer" ', '');
+      // Extract the SVG element from the rendered circular view
+      circularViewHtml = ReactDOMServer.renderToString(<App params={req.query} />);
+      const svgStartPos = circularViewHtml.indexOf('<svg') + '<svg'.length;
+      const svgEndPos = circularViewHtml.indexOf("</svg>") + "</svg>".length;
+      let svgElement = '<svg id="plasmidMap" ' + circularViewHtml.substring(svgStartPos, svgEndPos).trim();
 
-    // Get rid of titles
-    svgElement = svgElement.replaceAll(/<title.*>[\s\S]*?<\/title>/ig, '');
+      // Download file rather than displaying it
+      res.attachment(`${req.query.plasmidTitle ? req.query.plasmidTitle : 'plasmid'}.html`);
+      res.type('html');
 
-    // Download file rather than displaying it
-    res.attachment('plasmid.svg');
-    res.type('xml');
+      return res.send(
+        data.replace(
+          "<svg id='plasmidMap'></svg>",
+          svgElement
+        )
 
-    return res.send(
-      data.replace(
-        "<svg id='plasmidMap'></svg>",
-        svgElement
-      )
-    );
+      );
+    } catch (error) {
+      return res.status(500).send("An error occurred");
+    }
   });
 });
 
